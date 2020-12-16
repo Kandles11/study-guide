3699f999678fdeb30a7aec3aaac13553143f1662
Kandles11 <Kandleseatwicks@gmail.com>
2020-12-16 17:04:38 +0000

    add landing page for beta

--- study-guide/src/App.js
+++ study-guide/src/App.js
@@ -4,19 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header className="App-header"> 
+        <h1>Welcome to Study Guide 2.0!</h1>
+        <h2>We are way early into production, so please be patient!</h2>
+        <p>So the study-guide is switching to a dynamic site 🙌! This means easier uploading of answers, 
+            cool dynamic features like search bars and pdfs, and way more! But this also means an entire API has to be
+            created for it all to work. This API will be public, and of course, it will be easily accessable on this site.
+            Once the API is running, the react version of the site must be built. This will also take time. The current 
+            expected deadline is before 2021, but it is currently unknown when the transition will be 100% complete. Thank you
+            for using Mason's Cool Study Guide!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
