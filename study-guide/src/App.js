acd7ba99a9bde854db03e18744829c18f96749b8
Kandles11 <Kandleseatwicks@gmail.com>
2021-01-08 19:35:00 +0000

    delete old code

--- study-guide/src/App.js
+++ study-guide/src/App.js
@@ -7,6 +7,10 @@ function App() {
       <header className="App-header"> 
         <h1>Welcome to Study Guide 2.0!</h1>
         <h2>We are way early into production, so please be patient!</h2>
+        <code>Uh Oh! Mason had some massive delays while trying to get the site up and running, so
+            what that means is the dynamic features of the website will take a while longer. don't worry though! as soon as 
+            the new website it up, it will be updated and fixed! Thanks for yall's patience!
+        </code>
         <p>So the study-guide is switching to a dynamic site 🙌! This means easier uploading of answers, 
             cool dynamic features like search bars and pdfs, and way more! But this also means an entire API has to be
             created for it all to work. This API will be public, and of course, it will be easily accessable on this site.
