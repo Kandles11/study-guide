eccc05309d191e838a2e4c1d27a4653f118c3b1b
Mason Thomas <mason.thomas.0414@gmail.com>
2021-02-02 14:12:01 -0600

    change how json is formatted, move table to comp.

--- study-guide/src/components/data/usHist1302Data.js
+++ study-guide/src/components/data/usHist1302Data.js
@@ -1,35 +1,52 @@
 export const usHist1302Data = {
-  "voiceovers":[
-    {
-      "Question":"Chapter 32 Part 2 (2nd Half of Chapter)",
-      "Answer":""
-    },
-    {
-      "Question": "A group that was very angry about the masssive bailouts of huge banks and corporations after 07-08 econimic metldown and who wanted a radically smaller government was called",
-      "Answer": "the Tea Party"
-    },
-    {
-      "Question": "What 2013 Supreme Court Case gutted key provisions of the Voting Act of 1965, making it more difficult for certain groups to vote in certain areas?",
-      "Answer": "Shelby v. Holder"
-    },
-    {
-      "Question": "Why did the Occupy Wall Street movement begin to fade?",
-      "Answer": "It was leaderless, and had no specific goals"
-    },
-    {
-      "Question": "Who was called the \"Deporter in Chief\" because under his administration, record numbers of illegal aliens were deported",
-      "Answer": "Barack Obama"
-    },
-    {
-      "Question": "During the Arab Awakening, what became the main stumbling block for installing democratic governemnts",
-      "Answer": "the unwillingness to allow freedom of religion"
-    }
-  ],
-  "revel":[
-    {
-      "Question":"Chapter 32",
-      "Answer":""
-    },
+  "voiceovers":{
+    "Chapter 32":[
+      {
+        "Question": "A group that was very angry about the masssive bailouts of huge banks and corporations after 07-08 econimic metldown and who wanted a radically smaller government was called",
+        "Answer": "the Tea Party"
+      },
+      {
+        "Question": "What 2013 Supreme Court Case gutted key provisions of the Voting Act of 1965, making it more difficult for certain groups to vote in certain areas?",
+        "Answer": "Shelby v. Holder"
+      },
+      {
+        "Question": "Why did the Occupy Wall Street movement begin to fade?",
+        "Answer": "It was leaderless, and had no specific goals"
+      },
+      {
+        "Question": "Who was called the \"Deporter in Chief\" because under his administration, record numbers of illegal aliens were deported",
+        "Answer": "Barack Obama"
+      },
+      {
+        "Question": "During the Arab Awakening, what became the main stumbling block for installing democratic governemnts",
+        "Answer": "the unwillingness to allow freedom of religion"
+      }
+    ],
+    "Chapter 17":[
+      {
+        "Question": "What did the Munn v. Illinois case do?",
+        "Answer": "ruled that when a private company's business affected the public, the public has the right to regulate that business for the common good."
+      },
+      {
+        "Question": "What did the Wabash Case do?",
+        "Answer": "ruled that only Congress can regulate commerce between and among the states."
+      },
+      {
+        "Question": "What were 2 reasons why the cattle industry was stifled early on?",
+        "Answer": "because the Civil War began, because Missouri passed a law making it illegal to import cattle from Texas"
+      },
+      {
+        "Question": "In 1866, when the Transcontinental railroad reached Abilene, Kansas, who decided to take advantage of the situation and ship cattle on the Kansas Pacific Railroad?",
+        "Answer": "Joseph G. McCoy"
+      },
+      {
+        "Question": "What did the book, A Century of Dishonor, lead to?",
+        "Answer": "the Dawes Severalty Act"
+      }
+    ]
+  },
+  "revel":{
+    "Chapter 32": [
     {
       "Question": "The presidential election of 2000 hinged on the votes of which state?",
       "Answer": "Florida"
@@ -81,6 +98,206 @@ export const usHist1302Data = {
     {
       "Question": "In contrast to baby boomers, Millennials and Generation X get most of their news from __________.",
       "Answer": "Facebook"
-    }
-  ]
+    }],
+    "Chapter 18": [{
+      "Question": "By __________, U.S. manufacturing surpassed that of Britain, France, and Germany combined.",
+      "Answer": 1900
+    },
+    {
+      "Question": "The novelist William Dean Howells remarked, “It is in these things of iron and steel that the national genius most freely speaks.” Which of the following is an example of the “things” to which Howells refers?",
+      "Answer": "Corliss engine"
+    },
+    {
+      "Question": "Which of the following symbolized America’s status as an industrial nation?",
+      "Answer": "Machinery Hall"
+    },
+    {
+      "Question": "Which of the following groups participates in the “gig economy”?",
+      "Answer": "on-demand drivers for Uber"
+    },
+    {
+      "Question": "Which of the following characterized the railroads before the Civil War?",
+      "Answer": "They were designed more to protect local interests."
+    },
+    {
+      "Question": "Congress required which of the following to carry government freight, troops, and mail at reduced rates in exchange for federal subsidies?",
+      "Answer": "railroads"
+    },
+    {
+      "Question": "Which of the following was a trunk line in the late nineteenth century?",
+      "Answer": "the Baltimore and Ohio"
+    },
+    {
+      "Question": "As a result of the Panic of 1893, which of the following took control of the railroad industry?",
+      "Answer": "J. P. Morgan and Company"
+    },
+    {
+      "Question": "Which city became the center of the steel industry in the late nineteenth century?",
+      "Answer": "Pittsburgh"
+    },
+    {
+      "Question": "Petroleum led to the widespread use of which of the following in the early twentieth century?",
+      "Answer": "kerosene lamps"
+    },
+    {
+      "Question": "In 1882, John D. Rockefeller’s Standard Oil Company was organized as the first major __________.",
+      "Answer": "trust"
+    },
+    {
+      "Question": "Who was called the “Wizard of Menlo Park”?",
+      "Answer": "Thomas Alva Edison"
+    },
+    {
+      "Question": "What did American Sugar Refining, the Northern Securities Company, and the National Biscuit Company share in common?",
+      "Answer": "They all organized their own trusts."
+    },
+    {
+      "Question": "Which of the following most contributed to the advertising boom in the late nineteenth century?",
+      "Answer": "the rotary press"
+    },
+    {
+      "Question": "Which of the following people helped to make the department store into a national institution?",
+      "Answer": "John Wanamaker"
+    },
+    {
+      "Question": "Which of the following people entered the mail-order business in the late nineteenth century?",
+      "Answer": "Richard W. Sears"
+    },
+    {
+      "Question": "Which of the following reflects a trend in the last quarter of the nineteenth century that affected the lives of workers?",
+      "Answer": "Real wages rose."
+    },
+    {
+      "Question": "Which industry was the largest employer of women?",
+      "Answer": "textile"
+    },
+    {
+      "Question": "Of the groups listed, which one earned the most in the late nineteenth century?",
+      "Answer": "clerical workers"
+    },
+    {
+      "Question": "In contrast to the American Federation of Labor, the Cigar Makers’ Union and the Typographical Union __________.",
+      "Answer": "accepted women workers"
+    },
+    {
+      "Question": "Terence V. Powderly would agree with which of the following statements?",
+      "Answer": "Union membership should be open to all laborers regardless of race, gender, or skill."
+    },
+    {
+      "Question": "Which of the following correctly matches the person to the organization that he headed?",
+      "Answer": "Samuel Gompers – American Federation of Labor"
+    },
+    {
+      "Question": "Which of the following reflects the “iron law of wages”?",
+      "Answer": "Wages are dictated by supply and demand."
+    },
+    {
+      "Question": "Which of the following was involved in the Homestead Strike?",
+      "Answer": "Amalgamated Iron and Steel Workers"
+    },
+    {
+      "Question": "In the late nineteenth century, which part of the United States was the most industrialized?",
+      "Answer": "Northeast"
+    },
+    {
+      "Question": "In late nineteenth-century America, the government usually sided with which group in labor disputes?",
+      "Answer": "management"
+    },
+    {
+      "Question": "Which of the following characterized labor unions in late nineteenth-century America?",
+      "Answer": "They had an antagonistic relationship with the government."
+    }],
+    "Chapter 19": [{
+      "Question": "Many of the recently arrived immigrants who contributed to the urbanization of the United States in the late nineteenth century came from which part of the world?",
+      "Answer": "Asia"
+    },
+    {
+      "Question": "Which of the following contributed to the dramatic increase in the urban population in the late nineteenth century?",
+      "Answer": "migration from the countryside"
+    },
+    {
+      "Question": "Which group was most negatively affected by Jim Crow laws?",
+      "Answer": "African Americans"
+    },
+    {
+      "Question": "Which of the following expanded the city’s limits and transformed urban space in the late nineteenth century?",
+      "Answer": "streetcar systems"
+    },
+    {
+      "Question": "Which of the following would most likely have supported the mission of the Immigration Restriction League?",
+      "Answer": "American Protective Association"
+    },
+    {
+      "Question": "Which of the following is an example of an immigrant association?",
+      "Answer": "National Council of Jewish Women"
+    },
+    {
+      "Question": "Historically, anti-immigrant sentiment has been strongest during periods characterized by __________.",
+      "Answer": "economic downturns"
+    },
+    {
+      "Question": "What was the name of the Democratic Party organization that dominated city politics in New York from the 1850s to the 1930s?",
+      "Answer": "Tammany Hall"
+    },
+    {
+      "Question": "Which of the following did the Mugwumps support?",
+      "Answer": "civil-service reform"
+    },
+    {
+      "Question": "Which of the following signaled the rise of the city and new forms of leisure in the late nineteenth century?",
+      "Answer": "spectator sports"
+    },
+    {
+      "Question": "Which of the following people played an important role in the establishment of the National American Woman Suffrage Association in 1890?",
+      "Answer": "Susan B. Anthony"
+    },
+    {
+      "Question": "In which of the following cases did the Supreme Court apply the same rationale that had informed its ruling in Plessy v. Ferguson?",
+      "Answer": "Cumming v. County Board of Education"
+    },
+    {
+      "Question": "Whose strategy to achieve racial equality became known as the Atlanta Compromise?",
+      "Answer": "Booker T. Washington"
+    },
+    {
+      "Question": "What did Supreme Court decisions between 1878 and 1898 render ineffectual?",
+      "Answer": "Reconstruction amendments"
+    },
+    {
+      "Question": "What did the Jim Crow laws uphold?",
+      "Answer": "racial segregation"
+    },
+    {
+      "Question": "What did blacks who migrated to the North call the racism they encountered there?",
+      "Answer": "James Crow"
+    },
+    {
+      "Question": "Herbert Spencer developed the theory of __________.",
+      "Answer": "social selection"
+    },
+    {
+      "Question": "Who proposed a “single tax” on the “unearned increment” of land as a solution to economic inequality?",
+      "Answer": "Henry George"
+    },
+    {
+      "Question": "Which of the following did settlement houses offer?",
+      "Answer": "medical care"
+    },
+    {
+      "Question": "Of the following people, whose ideas most influenced Clarence Darrow?",
+      "Answer": "Henry George"
+    },
+    {
+      "Question": "Which part of the United States was the most urbanized in 1900?",
+      "Answer": "Northeast"
+    },
+    {
+      "Question": "By the end of the nineteenth century, there was a general sense among Americans that __________ had disappeared.",
+      "Answer": "the frontier"
+    },
+    {
+      "Question": "In 1900, the immigrant population in Texas was concentrated along the border with __________.",
+      "Answer": "Mexico"
+    }],
+  }
 };
--- study-guide/src/components/usHist1302.js
+++ study-guide/src/components/usHist1302.js
@@ -2,6 +2,8 @@ import react from 'react';
 import '../index.css'
 import { usHist1302Data } from './data/usHist1302Data.js'
 
+import Table from './Table'
+
 
 
 function usHist1302() {
@@ -10,19 +12,18 @@ function usHist1302() {
       <body> 
         <h1>US History 1302!</h1>
         <h2>Voice-Overs</h2>
-        <table>
-          <tr><th>Question</th><th>Answer</th></tr>
-          {usHist1302Data.voiceovers.map(data => (
-          <tr><td>{data.Question}</td><td>{data.Answer}</td></tr>
-        ))}
-        </table>
+          <h3>Chapter 32 VO</h3>
+          <Table json={usHist1302Data.voiceovers['Chapter 32']} />
+          <h3>Chapter 17 VO</h3>
+          <Table json={usHist1302Data.voiceovers['Chapter 17']} />
         <h2>Revel</h2>
-        <table>
-          <tr><th>Question</th><th>Answer</th></tr>
-          {usHist1302Data.revel.map(data => (
-          <tr><td>{data.Question}</td><td>{data.Answer}</td></tr>
-        ))}
-        </table>
+          <h3>Chapter 32 RE</h3>
+          <Table json={usHist1302Data.revel['Chapter 32']} />
+          <h3>Chapter 18 RE</h3>
+          <Table json={usHist1302Data.revel['Chapter 18']} />
+          <h3>Chapter 19 RE</h3>
+          <Table json={usHist1302Data.revel['Chapter 19']} />
+          <h3>Chapter 20 RE</h3>
       </body>
     </div>
   );
