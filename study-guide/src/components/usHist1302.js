faf7d611b4aa62aaebe2ecea6e4b612856307a43
Kandles11 <Kandleseatwicks@gmail.com>
2021-02-03 21:00:00 +0000

    add css

--- study-guide/src/components/usHist1302.js
+++ study-guide/src/components/usHist1302.js
@@ -11,10 +11,21 @@ function usHist1302() {
     <div className="TestPage">
       <body> 
         <h1>US History 1302!</h1>
+        <div class="flexParent">
+        <div class="flexChild">
+            <h2>Voiceovers:</h2>
+            <a href="#Chapter32VO">Chapter 32</a>
+            <a href="#Chapter17VO">Chapter 17</a>
+        </div>
+        <div class="flexChild">
+            <h2>Revel Answers:</h2>
+            <a href="#Chapter32RE">Chapter 32</a>
+        </div>
+        </div>
         <h2>Voice-Overs</h2>
-          <h3>Chapter 32 VO</h3>
+          <h3 id="Chapter32VO">Chapter 32 VO</h3>
           <Table json={usHist1302Data.voiceovers['Chapter 32']} />
-          <h3>Chapter 17 VO</h3>
+          <h3 id="Chapter17VO">Chapter 17 VO</h3>
           <Table json={usHist1302Data.voiceovers['Chapter 17']} />
         <h2>Revel</h2>
           <h3>Chapter 32 RE</h3>
