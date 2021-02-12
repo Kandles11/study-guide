7d07dac6fef5a29af2b2fda9986f55c170d36a37
Mason Thomas <mason.thomas.0414@gmail.com>
2021-02-12 00:23:21 -0600

    maybe added offline cache i dont really know?

--- study-guide/src/index.js
+++ study-guide/src/index.js
@@ -6,7 +6,7 @@ import {
   Route,
   Link
 } from "react-router-dom";
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './index.css';
 
 import App from './components/App';
@@ -29,3 +29,7 @@ ReactDOM.render(
 );
 
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorkerRegistration.register();
