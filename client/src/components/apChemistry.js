b98d179a8eb4b91ec625f7e6eb5075cb6a8af1e6
Kandles11 <Kandleseatwicks@gmail.com>
2021-02-23 17:19:14 +0000

    try to fix pdf viewing, doesn't work

---
+++ client/src/components/apChemistry.js
@@ -0,0 +1,45 @@
+import react from 'react';
+import '../index.css'
+import { pdfjs } from 'react-pdf';
+import { Document, Page } from 'react-pdf';
+
+
+
+import Table from './Table'
+import Header from './Header'
+import Footer from './Footer'
+
+
+
+function apChemistry() {
+    pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+    
+  return (
+    <div className="usGov2305">
+      <body> 
+        <main>
+        <Header text="AP Chemistry 🧪" />
+        <div class="flexParent">
+        <div class="flexChild">
+            <h2>PDF's:</h2>
+            <a href="#Chapter1MI">PDF 1</a>
+        </div>
+        </div>
+        <h2>PDF's</h2>
+        <Document
+        options={{
+                cMapUrl: `//cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/cmaps/`,
+    cMapPacked: true,
+        }}
+        file="http://s23.q4cdn.com/479936946/files/doc_downloads/test.pdf"      >
+      </Document>
+        </main>
+        <div class="push"/>
+        <Footer />
+      </body>
+    </div>
+  );
+}
+
+export default apChemistry;
--- client/src/index.js
+++ client/src/index.js
@@ -12,6 +12,7 @@ import App from './components/App';
 import usHist1302 from './components/usHist1302';
 import usHist1301 from './components/usHist1301';
 import usGov2305 from './components/usGov2305';
+import apChemistry from './components/apChemistry'
 
 
 ReactDOM.render(
@@ -21,7 +22,7 @@ ReactDOM.render(
       <Route path="/usHist1302" component={usHist1302} />
       <Route path="/usHist1301" component={usHist1301} />
       <Route path="/usGov2305" component={usGov2305} />
-
+    <Route path="/apChemistry" component={apChemistry} />
     </div>
   </Router>,
   document.getElementById('root')
